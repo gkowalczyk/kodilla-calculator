dd1e73611a65ba09acc5877f3a579026cc91b222
Grzegorz <grzegorzkowalczyk1989@gmail.com>
2021-09-30 19:40:33 +0200

    first commit 2.1

---
+++ src/main/java/com/kodilla/Test.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+class Test {
+
+    public int getSum(int a, int b) {
+        return a + b;
+    }
+
+    public int getSub(int a, int b) {
+        return a - b;
+    }
+}
--- src/main/java/com/kodilla/calculator.java
+++ src/main/java/com/kodilla/calculator.java
@@ -1,21 +1,12 @@
 package com.kodilla;
 
-class Test {
-
-    public int getSum(int a, int b) {
-        return a + b;
-    }
-        public int getSub(int a, int b){
-            return  a-b;
-    }
-}
-
 public class calculator {
     public static void main(String[] args) {
         Test test = new Test();
        int r1 = test.getSum(1,2);
        int r2 =  test.getSub(1,2);
         System.out.println("sum="+ r1);
+        System.out.println("sub="+ r2);
         System.out.println("sub="+ r2);
     }
 }
